6c0d8847821626aa2c39d247a1cd972990475eea
Anthony Cossins <anthonyecossins@gmail.com>
2020-05-03 20:40:12 +0100

    Hack together animation

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function createSegmentDisplay(svgElement, userOptions = {}) {
-  const defaultOptions = { inactiveColor: 'grey', activeColor: 'red' };
+  const defaultOptions = { inactiveColor: 'grey', activeColor: 'red', hideSegments: [] };
   const options = { ...defaultOptions, ...userOptions};
 
   const font = {
+    ' ': [],
     'A': [0, 1, 2, 3, 4, 5, 9, 13],
     'B': [2, 3, 4, 5, 6, 7, 11, 13, 15],
     'I': [2, 3, 6, 7, 11, 15],
     'N': [0, 1, 4, 5, 10, 14],
     'O': [0, 1, 2, 3, 4, 5, 6, 7],
     'R': [0, 1, 2, 3, 4, 9, 13, 14],
+    'R.': [0, 1, 2, 3, 4, 9, 13, 14, 16],
     'CURL1': [0, 1, 2, 10, 11],
     'CURL2': [1, 2, 3, 4, 12, 13],
     'CURL3': [4, 5, 6, 14, 15],
@@ -17,7 +19,11 @@ function createSegmentDisplay(svgElement, userOptions = {}) {
 
   const groups = svgElement.querySelectorAll('g');
   const polygons = Array.from(groups).map((group) => {
-    return group.querySelector('polygon');
+    return group.querySelector('polygon') || group.querySelector('circle');
+  });
+
+  options.hideSegments.forEach((index) => {
+    polygons[index].style.opacity = '0';
   });
 
   const segmentDisplay = {
@@ -40,19 +46,23 @@ function createSegmentDisplay(svgElement, userOptions = {}) {
     },
     showSequence: function (charSequence, sequenceOptions = { delay: 100, loop: 0 }) {
       let index = 0;
+      let interval;
 
-      setInterval(() => {
+      interval = setInterval(() => {
         const char = charSequence[index];
 
         this.show(char);
 
         if (index < charSequence.length - 1) {
           index++;
+        } else {
+          if (sequenceOptions.loop) {
+            index = 0;
+          } else {
+            clearInterval(interval);
+          }
         }
 
-        if (sequenceOptions.loop) {
-          index = 0;
-        }
       }, sequenceOptions.delay);
     }
   }
@@ -79,16 +89,41 @@ const sequences = [
   ['CURL1', 'CURL2', 'CURL3', 'CURL4', 'CURL1', 'CURL2', 'CURL3', 'CURL4', 'CURL1', 'R']
 ];
 
+const endSequenceForR = ['R', 'R.'];
+const displays = [];
+
 svgs.forEach((svg, index) => {
   const display = createSegmentDisplay(svg, {
-    inactiveColor: '#323132',
-    activeColor: '#FBAE17'
+    inactiveColor: '#111111',
+    activeColor: '#FBAE17',
+    hideSegments: index === 4 ? [] : [16]
   });
+
+  displays.push(display);
 
   setTimeout(() => {
     display.showSequence(sequences[index], {
       delay: 80
     });
-  }, 100 * index)
+  }, 200 * index);
+
+  if (index !== svgs.length - 1) {
+    return;
+  }
+
+  setTimeout(() => {
+    display.showSequence(endSequenceForR, {
+      delay: 400,
+      loop: true
+    });
+  }, (200 * svgs.length) + 500);
 });
+
+setTimeout(() => {
+  displays.forEach((display, index) => {
+    setTimeout(() => {
+      display.showSequence(['CURL1', 'CURL2', 'CURL3', 'CURL4', 'CURL1', 'CURL2', 'CURL3', 'CURL4', ' ']);
+    }, 200 * index);
+  });
+}, 5000);
 
